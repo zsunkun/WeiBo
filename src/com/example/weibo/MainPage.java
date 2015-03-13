40a20f323d6940507d7ed15ef51f77576f0fa1cc
sunkun <zsunkun@163.com>
2015-03-13 10:47:35 +0800

    feature:first commit

---
+++ src/com/example/weibo/MainPage.java
@@ -0,0 +1,14 @@
+package com.example.weibo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_page);
+	}
+
+}
