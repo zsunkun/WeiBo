20376dda2b43179c3b8fefa51e91444646c2f50f
sunkun <zsunkun@163.com>
2015-03-27 18:02:57 +0800

    change:change the way of slideMenu

--- src/com/example/ui/SlidingMenu.java
+++ src/com/example/ui/SlidingMenu.java
@@ -2,15 +2,16 @@ package com.example.ui;
 
 import com.example.utils.DisplayUtils;
 
+import android.R.menu;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
-public class SlidingMenu extends HorizontalScrollView {
+public class SlidingMenu extends FrameLayout {
 
 	private int mScreenWidth;
 	private int mMenuRightPadding = 50;
@@ -18,6 +19,9 @@ public class SlidingMenu extends HorizontalScrollView {
 	private int mMenuWidth;
 	private int mHalfMenuWidth;
 	private boolean mIsMenuOpen;
+	private ViewGroup mMenu;
+	private ViewGroup mContent;
+	private LinearLayout.LayoutParams mMenuParams;
 
 	private boolean mFirst = true;
 
@@ -30,16 +34,17 @@ public class SlidingMenu extends HorizontalScrollView {
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		if (mFirst) {
 			LinearLayout wrapper = (LinearLayout) getChildAt(0);
-			ViewGroup menu = (ViewGroup) wrapper.getChildAt(0);
-			ViewGroup content = (ViewGroup) wrapper.getChildAt(1);
+			mMenu = (ViewGroup) wrapper.getChildAt(0);
+			mContent = (ViewGroup) wrapper.getChildAt(1);
 			// dp to px
 			mMenuRightPadding = DisplayUtils.getPixel(getContext(),
 					mMenuRightPadding);
 			mMenuWidth = mScreenWidth - mMenuRightPadding;
 			mSafeWidth = DisplayUtils.getPixel(getContext(), mSafeWidth);
 			mHalfMenuWidth = mMenuWidth / 2;
-			menu.getLayoutParams().width = mMenuWidth;
-			content.getLayoutParams().width = mScreenWidth;
+			mMenu.getLayoutParams().width = mMenuWidth;
+			mContent.getLayoutParams().width = mScreenWidth;
+			mMenuParams = (LinearLayout.LayoutParams) mMenu.getLayoutParams();
 
 		}
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -51,7 +56,9 @@ public class SlidingMenu extends HorizontalScrollView {
 		super.onLayout(changed, l, t, r, b);
 		if (changed) {
 			// ½«²Ëµ¥Òþ²Ø
-			this.scrollTo(mMenuWidth, 0);
+			// this.scrollTo(mMenuWidth, 0);
+			mMenuParams.leftMargin = -mMenuWidth;
+			mIsMenuOpen = false;
 			mFirst = false;
 		}
 	}
@@ -59,43 +66,44 @@ public class SlidingMenu extends HorizontalScrollView {
 	float startX = 0;
 	float endX = 0;
 
-	@Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		int action = ev.getAction();
-		switch (action) {
-		case MotionEvent.ACTION_UP:
-			int scrollX = getScrollX();
-			if (scrollX > mHalfMenuWidth) {
-				this.smoothScrollTo(mMenuWidth, 0);
-				mIsMenuOpen = false;
-			} else {
-				this.smoothScrollTo(0, 0);
-				mIsMenuOpen = true;
-			}
-			return true;
-		case MotionEvent.ACTION_MOVE:
-			endX = ev.getX();
-			if (Math.abs(endX - startX) < mSafeWidth) {
-				System.out.println("Math:---" + Math.abs(endX - startX));
-				return true;
-			}
-			break;
-		case MotionEvent.ACTION_DOWN:
-			startX = ev.getX();
-			System.out.println("-----"+startX);
-			break;
-		}
-		return super.onTouchEvent(ev);
-	}
+	// @Override
+	// public boolean onTouchEvent(MotionEvent ev) {
+	// int action = ev.getAction();
+	// switch (action) {
+	// case MotionEvent.ACTION_UP:
+	// int scrollX = getScrollX();
+	// if (scrollX > mHalfMenuWidth) {
+	// this.smoothScrollTo(mMenuWidth, 0);
+	// mIsMenuOpen = false;
+	// } else {
+	// this.smoothScrollTo(0, 0);
+	// mIsMenuOpen = true;
+	// }
+	// return true;
+	// case MotionEvent.ACTION_MOVE:
+	// endX = ev.getX();
+	// if (Math.abs(endX - startX) > mSafeWidth) {
+	// //TODO
+	// }
+	// break;
+	// case MotionEvent.ACTION_DOWN:
+	// startX = ev.getX();
+	// break;
+	// }
+	// return super.onTouchEvent(ev);
+	// }
 
 	public void toggleMenu() {
 		if (mIsMenuOpen) {
-			this.smoothScrollTo(mMenuWidth, 0);
+			// this.scrollTo(mMenuWidth, 0);
+			mMenuParams.leftMargin = -mMenuWidth;
+			mMenu.setLayoutParams(mMenuParams);
 			mIsMenuOpen = false;
 		} else {
-			this.smoothScrollTo(0, 0);
+			// this.scrollTo(0, 0);
+			mMenuParams.leftMargin = 0;
+			mMenu.setLayoutParams(mMenuParams);
 			mIsMenuOpen = true;
 		}
-
 	}
 }
