45b52130c58bca9ca89675205835824081259e6b
zsunkun <zsunkun@163.com>
2015-04-12 22:45:45 +0800

    change:change splash  page UI

---
+++ src/com/example/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.utils;
+
+public interface Constants {
+
+	public static final String APP_KEY = "2045436852";
+	public static final String REDIRECT_URL = "http://www.sina.com";
+}
--- src/com/example/weibo/SplashActivity.java
+++ src/com/example/weibo/SplashActivity.java
@@ -3,25 +3,43 @@ package com.example.weibo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
-	private Runnable mRunnable = new Runnable() {
-		public void run() {
-			startActivity(new Intent(SplashActivity.this, MainPage.class));
-			finish();
-		}
-	};
-
-	private Handler mHandler = new Handler();
-
+	private ImageView mSplashLogo;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_page);
 		overridePendingTransition(R.anim.fade_in, R.anim.fade_out_fast);
-		mHandler.postDelayed(mRunnable, 1500);
+		mSplashLogo = (ImageView) findViewById(R.id.image_splash_logo);
+		AlphaAnimation animation = new AlphaAnimation(0.1f, 1.0f);
+		animation.setDuration(3000);
+		mSplashLogo.setAnimation(animation);
 
+		animation.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				startActivity(new Intent(SplashActivity.this, MainPage.class));
+				finish();
+			}
+
+		});
 	}
+	
 }
